5d7cecc5cb3ea543fb336919fb10602d8e3c5def
Andika Octavia Pratama Putra <andikaopp@gmail.com>
2023-09-19 14:05:38 +0700

    add logout user and unit test

--- src/main/java/developerkeren/restful/service/AuthService.java
+++ src/main/java/developerkeren/restful/service/AuthService.java
@@ -46,4 +46,12 @@ public class AuthService {
     private Long next60Days(){
         return System.currentTimeMillis() + (1000 * 60 * 24 * 60);
     }
+
+    @Transactional
+    public void logout(User user){
+        user.setToken(null);
+        user.setTokenExpiredAt(null);
+
+        userRepository.save(user);
+    }
 }
